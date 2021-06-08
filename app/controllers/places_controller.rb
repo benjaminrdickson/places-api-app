807e3722d443d36d76906ac09c765b835354f233
benjaminrdickson <bdickson8619@gmail.com>
2021-06-08 10:55:30 -0500

    initial commit

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,47 @@
+class PlacesController < ApplicationController
+
+  def index
+    places = Place.all
+    render json: places
+  end 
+
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address]
+    )
+    place.save
+    if place.save
+      render json: place
+    else 
+      render json: {errors: place.errors.full_messages }, status: :unprocessible_entity
+    end 
+  end 
+
+  def show
+    place = Place.find(params[:id])
+    render json: place
+  end 
+
+  def update 
+    place = Place.find(params[:id])
+    place.name = params[:name] || place.name
+    place.address = params[:address] || place.address 
+    place.save
+    if place.save
+      render json: place
+    else 
+      render json: {message: place.errors.full_messages}, status: :unprocessible_entity
+    end 
+  end 
+
+  def destroy 
+    place = Place.find(params[:id])
+    place.destroy
+    render json: {message: "Place Destroyed"}
+  end 
+
+
+
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+places = Place.create!([
+  {name: "Cescas", address: "5403 N Clark St, Chicago, IL 60640"},
+  {name: "Moda Center", address: "1 N Center Ct St, Portland, OR 97227"}
+
+])
